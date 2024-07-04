c89204e618bc4c311b0b9e439992f1cadb10dde6
Cool <990299103@qq.com>
2024-07-05 03:51:35 +0800

    feat：“修改添加秒杀卷，及其逻辑的一些bug”

--- src/main/java/com/example/HmDianPingApplication.java
+++ src/main/java/com/example/HmDianPingApplication.java
@@ -3,7 +3,9 @@ package com.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @MapperScan("com.example.mapper")
 @SpringBootApplication
 public class HmDianPingApplication {
