2291b2e7e938bbf075c6f095cb53acb87d940923
Cool <990299103@qq.com>
2024-07-27 01:50:09 +0800

    feat："添加用户退出登录清除token功能"

--- src/main/java/com/example/service/IUserService.java
+++ src/main/java/com/example/service/IUserService.java
@@ -5,6 +5,7 @@ import com.example.dto.LoginFormDTO;
 import com.example.dto.Result;
 import com.example.entity.User;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -20,4 +21,6 @@ public interface IUserService extends IService<User> {
     Result sendCode(String phone, HttpSession session);
 
     Result login(LoginFormDTO loginForm, HttpSession session);
+
+    Result logout(HttpServletRequest request);
 }
